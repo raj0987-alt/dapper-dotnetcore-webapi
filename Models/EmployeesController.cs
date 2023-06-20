947759ae5f30cf4be53478ae35d0a5667ef6c1b6
raj0987-alt <71288207+raj0987-alt@users.noreply.github.com>
2023-06-20 11:13:52 +0600

    Gell all data

---
+++ Models/EmployeesController.cs
@@ -0,0 +1,27 @@
+﻿using ASP.NETCore6WebAPICRUDWithEntityFramework_CodeFirstApproach_.Models;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace DapperCrudWebAPI.Models
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        public readonly IConfiguration _config;
+        public EmployeesController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Employee>>> GetAllEmployee()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DBConnection"));
+            var employees = await connection.QueryAsync<Employee>("SELECT * FROM Employees");
+            return Ok(employees);
+        }
+    }
+}
